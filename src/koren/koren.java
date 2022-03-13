d5d13b13f002089764d6a913029b035c772de8b8
pmetodiev <plame478@gmail.com>
2022-03-13 12:02:44 +0200

    koren

---
+++ src/koren/koren.java
@@ -0,0 +1,14 @@
+package koren;
+
+public class koren {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(8));
+        System.out.println(Math.sqrt(353));
+        System.out.println(Math.sqrt(78));
+        System.out.println(Math.sqrt(3));
+        System.out.println(Math.sqrt(-67));
+        System.out.println(Math.sqrt(723));
+        System.out.println(Math.sqrt(2000));
+        System.out.println(Math.sqrt(-100));
+    }
+}
